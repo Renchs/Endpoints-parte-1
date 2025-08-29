3125a5a6afd0d57db1048f4d9476893495922e14
Renchs <mathfm.dev@gmail.com>
2025-08-29 14:25:38 -0300

    exercicio resolvido
    Co-authored-by: Thainá Lopes <thaina-lopes@users.noreply.github.com>
    Co-authored-by: Mariana Azevedo da Silva <Mariana-Azevedo@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import { usuariosRoutes } from "./routes/usuarios";
 
-const app = express();
+export const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/", usuariosRoutes); 
+app.use("/", usuariosRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Esse servidor está rodando em ${port}`);
--- src/routes/usuarios.ts
+++ src/routes/usuarios.ts
@@ -1,7 +1,46 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import db from "../client/db";
 
-const router = Router();
+export const usuariosRoutes = Router();
+
+const create = (req: Request, res: Response) => {
+    const user = req.body;
+    const newUser = db.create(user);
+    res.send(newUser).status(201);
+}
+
+const findAll = (req: Request, res: Response) => {
+    const users = db.findAll();
+    res.send(users).status(200);
+}
+
+const findById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const user = db.findById(id);
+    res.send(user).status(200);
+}
+
+const updateById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const user = req.body;
+
+    const updateUser = db.updateById(id, user);
+    res.send(updateUser).status(200);
+
+}
+
+const deleteById = (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  
+  res.send(db.remove(id)).status(200);
+};
+
+usuariosRoutes.get("/", findAll);
+usuariosRoutes.get("/:id", findById);
+usuariosRoutes.post("/", create);
+usuariosRoutes.put("/:id", updateById);
+usuariosRoutes.delete("/:id", deleteById);
+
 
 // Exercicio de CRUD
 // Utilizando as 5 funções encontradas em db, crie 5 endpoints para o recurso "usuario".
